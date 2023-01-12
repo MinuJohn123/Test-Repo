73bc1a2851587b453e90e24e3191344c1fa3376e
lenovo <engnoham@gmail.com>
2023-01-12 09:31:09 +0200

    Search by employee name testcase

--- src/main/java/Pages/EmployeePage.java
+++ src/main/java/Pages/EmployeePage.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -9,19 +10,28 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class EmployeePage {
 
     public WebDriverWait wait;
     JavascriptExecutor js;
     private Actions actions;
 
-    @FindBy(xpath = "//*[@class=\"mdi mdi-menu\"]")
+    //@FindBy(xpath = "//i[@class=\"mdi mdi-menu\"]")
+
+    //@FindBy(xpath="//*[@class=\"sideMenuToggle\"]")
+    @FindBy(xpath = "//a[@class=\"sideMenuToggle\"] //i[@class='mdi mdi-menu']")
     private WebElement homeMnu;
 
-    @FindBy(xpath = "//*[@class=\"input-style ng-pristine ng-valid ng-touched\"]")
+    @FindBy(xpath = "//*[@href=\"/hr/emp\"]")
+    private WebElement empMnu;
+
+    @FindBy(xpath = "//*[@class=\"input-style ng-untouched ng-pristine ng-valid\"]")
     private WebElement searchTxt;
 
-
+    @FindBy(xpath = "//*[@data-kendo-grid-column-index=\"1\"]")
+    private List<WebElement> grdName;
 
     public EmployeePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -30,11 +40,19 @@ public class EmployeePage {
         js = (JavascriptExecutor) driver;
     }
 
-    public void Search()
-    {
-        searchTxt.sendKeys("noha");
-        wait.until(ExpectedConditions.elementToBeClickable(homeMnu));
-        homeMnu.click();
+    public String SearchByName(String strSearh) {
+        wait.until(ExpectedConditions.visibilityOf(homeMnu));
+        js.executeScript("arguments[0].click()", homeMnu);
+        js.executeScript("arguments[0].click()", empMnu);
+        js.executeScript("arguments[0].click()", searchTxt);
+        searchTxt.sendKeys(strSearh);
+        String searchName = "";
+        //System.out.println("size is "+grdName.size());
+        if (grdName.size() > 0) {
+            //System.out.println("element 0 is "+Grd.get(0).getText());
+            searchName = grdName.get(0).getText();
+        }
+        return searchName;
     }
 
 
--- src/test/java/TestCases/EmployeeTest.java
+++ src/test/java/TestCases/EmployeeTest.java
@@ -3,20 +3,22 @@ package TestCases;
 import Heplers.JsonReader;
 import Pages.EmployeePage;
 import Pages.LoginPage;
+import com.google.gson.JsonObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class EmployeeTest extends BaseTest {
 
     private EmployeePage employeePage;
-
     private LoginPage loginPage;
+    private JsonObject employeeInfo;
 
 
     @BeforeClass
     public void init() {
-
         JsonReader jsonReader = new JsonReader();
+        employeeInfo = jsonReader.getJsonObject(System.getProperty("path.employeeInfo"));
         employeePage = new EmployeePage(webDriver);
     }
 
@@ -25,18 +27,18 @@ public class EmployeeTest extends BaseTest {
         //open application URL
         webDriver.get(System.getProperty("target.homepage"));
         // initialize an object from login page
-        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage = new LoginPage(webDriver);
         // login with username and password provided from test.properties file
         loginPage.Login(System.getProperty("account.username"), System.getProperty("account.password"));
-
-
     }
 
     @Test(priority = 1)
-    public void assertSearchTest() {
-         employeePage.Search();
+    public void assertSearchByNameTest() {
+        String searchName = employeeInfo.get("SearchName").getAsString();
+        String result = employeePage.SearchByName(searchName);
+        System.out.println("result : " + result);
+        Assert.assertEquals(result, searchName);
+        System.out.println("assertSearchTest is done successfully");
     }
-
-
 
 }
